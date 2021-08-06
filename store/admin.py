32f3624424a0d5f4100f58b529fe3cb9927c32ce
Khaled El Hamzi <contact@elhamzi.me>
2021-08-07 00:06:50 +0200

    add a search field & lookup type

--- store/admin.py
+++ store/admin.py
@@ -55,9 +55,10 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership', 'orders_count']
-    ordering = ['first_name', 'last_name']
     list_editable = ['membership']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
 
     @admin.display(ordering="orders_count")
