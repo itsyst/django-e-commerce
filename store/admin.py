7208fe93e88c195dc13f9327fa55de1dffe9d743
Khaled El Hamzi <contact@elhamzi.me>
2021-08-06 20:28:29 +0200

    add & sort computed product columns

--- store/admin.py
+++ store/admin.py
@@ -7,14 +7,21 @@ admin.site.register(models.Collection)
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'unit_price']
+    list_display = ['id', 'title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
-# admin.site.register(models.Product, ProductAdmin)
+   # admin.site.register(models.Product, ProductAdmin)
 
-@admin.register(models.Customer) 
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'Ok'
+
+
+@admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name','last_name','membership' ]
+    list_display = ['first_name', 'last_name', 'membership']
     ordering = ['first_name', 'last_name']
     list_editable = ['membership']
     list_per_page = 10
