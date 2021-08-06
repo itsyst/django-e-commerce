ffb8d5798cdd6b3f627aa6391902c5e4541be875
Khaled El Hamzi <contact@elhamzi.me>
2021-08-06 18:48:51 +0200

    change admin site header

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,6 +17,8 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'E-Commerce Administration'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
