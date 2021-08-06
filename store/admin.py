795b86daf31a0a880ec08e4c2ef25c6d76c515ed
Khaled El Hamzi <contact@elhamzi.me>
2021-08-06 19:54:41 +0200

    customize customer for site admin

--- store/admin.py
+++ store/admin.py
@@ -10,5 +10,11 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'unit_price']
     list_editable = ['unit_price']
     list_per_page = 10
-
 # admin.site.register(models.Product, ProductAdmin)
+
+@admin.register(models.Customer) 
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name','last_name','membership' ]
+    ordering = ['first_name', 'last_name']
+    list_editable = ['membership']
+    list_per_page = 10
