fc3f6cd20701c4000b7c9ca27c9b717194998bef
Khaled El Hamzi <contact@elhamzi.me>
2021-08-06 20:35:22 +0200

    add a related product object column

--- store/admin.py
+++ store/admin.py
@@ -7,10 +7,14 @@ admin.site.register(models.Collection)
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'unit_price', 'inventory_status']
+    list_display = ['id', 'title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
    # admin.site.register(models.Product, ProductAdmin)
+
+    def collection_title(self, product):
+      return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
